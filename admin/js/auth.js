7a81743d63da4180f9881fa34a7e4cf01f9de1c0
globiaworld <josedrash22@gmail.com>
2026-06-13 20:13:59 +0400

    Update auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,1 +1,33 @@
+```javascript
+async function login(){
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+const { error } =
+await supabase.auth.signInWithPassword({
+
+email,
+
+password
+
+});
+
+if(error){
+
+document.getElementById("status").innerHTML =
+error.message;
+
+return;
+
+}
+
+window.location =
+"dashboard.html";
+
+}
+```
 
